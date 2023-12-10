d881c72068b989712836f861dbc33d3d023f76e9
Ryan <ryanroudaut8@gmail.com>
2023-12-10 20:14:05 +0100

    js structure du jeu

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const cols = document.querySelectorAll(".col1, .col2, .col3, .col4, .col5, .col6, .col7");
+    const btnClear = document.getElementById("clear")
+    
+    function play() {
+        cols.forEach(col => {
+            col.addEventListener("click", () => {
+                col.style.backgroundColor = "red";
+            });
+        });
+        
+    }
+
+    function clear(){
+        btnClear.addEventListener("click", () => {
+            cols.forEach(col => {
+                col.style.backgroundColor = "white";
+            });
+        });
+    }
+    
+    play();
+    clear();
+
+});
+
+
